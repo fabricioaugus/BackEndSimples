807e69ef4476972b941d7199f2641caec5df30e3
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2022-04-05 19:26:49 -0200

    ...

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const importDb = require("./db.json")
+let port = process.env.PORT || 3001;
+app.get("/",(req,res)=>{
+    res.send("OLA");
+});
+app.get("/products",(req,res)=>{
+    res.send(importDb);
+});
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`);
+})
