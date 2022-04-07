a870aca2151ab84f835c9bc46000bb6753914ddb
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2022-04-07 17:27:01 -0200

    ...

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const importDb = require("./db.json")
 let port = process.env.PORT || 3001;
 app.get("/",(req,res)=>{
     res.send(importDb);
 });
-
 app.listen(port, ()=>{
     console.log(`http://localhost:${port}`);
 })
+*/
